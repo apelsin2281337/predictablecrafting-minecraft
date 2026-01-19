e3c89288b30bcb4ccc5e832d0a22fd48e593aebb
apelsin2281337 <ggwp.gg6578@gmail.com>
2026-01-19 17:46:49 +0300

    The result slot now displays the number of stacks being crafted along with the number of items

--- src/client/java/org/apelsin/bettercrafting/client/mixins/CraftingTableSlotMixin.java
+++ src/client/java/org/apelsin/bettercrafting/client/mixins/CraftingTableSlotMixin.java
@@ -3,31 +3,23 @@ package org.apelsin.bettercrafting.client.mixins;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.RecipeInputInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.CraftingResultSlot;
 import net.minecraft.screen.slot.Slot;
 import org.apelsin.bettercrafting.mixins.CraftingResultSlotAccessor;
 import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+
+
 @Environment(EnvType.CLIENT)
 @Mixin(Slot.class)
-public class CraftingTableMixin {
-    @Final
-    @Shadow
-    public Inventory inventory;
-
-    @Shadow
-    public int getIndex() {
-        return 0;
-    }
+public class CraftingTableSlotMixin {
 
     @Inject(
             method = "getStack",
@@ -73,10 +65,6 @@ public class CraftingTableMixin {
         if (minAmountOfItems == Integer.MAX_VALUE || minAmountOfItems <= 1) {
             return;
         }
-
-        long totalCraftableLong = (long) initialResult.getCount() * minAmountOfItems;
-        int totalCraftable = totalCraftableLong > Integer.MAX_VALUE ?
-                Integer.MAX_VALUE : (int) totalCraftableLong;
 
         ItemStack modifiedResult = initialResult.copy();
         modifiedResult.setCount((initialResult.getCount() * minAmountOfItems));
---
+++ src/client/java/org/apelsin/bettercrafting/client/mixins/DrawContextMixin.java
@@ -0,0 +1,51 @@
+package org.apelsin.bettercrafting.client.mixins;
+
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.item.ItemStack;
+import org.joml.Matrix3x2fStack;
+import org.jspecify.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(DrawContext.class)
+public class DrawContextMixin {
+    @Inject(method = "drawStackCount", at = @At("HEAD"), cancellable = true)
+    private void customStackCount(TextRenderer textRenderer, ItemStack stack, int x, int y, @Nullable String stackCountText, CallbackInfo ci) {
+        if (stack.getCount() != 1 || stackCountText != null) {
+            String string = stackCountText == null ? String.valueOf(stack.getCount()) : stackCountText;
+            DrawContext context = (DrawContext)(Object)this;
+
+            context.drawText(textRenderer, string, x + 19 - 2 - textRenderer.getWidth(string), y + 6 + 3, -1, true);
+
+            long windowHandle = MinecraftClient.getInstance().getWindow().getHandle();
+            boolean shiftPressed = GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_LEFT_SHIFT) == GLFW.GLFW_PRESS ||
+                    GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS;
+
+            if (shiftPressed && stack.getCount() > 64) {
+                Matrix3x2fStack matrices = context.getMatrices();
+                matrices.pushMatrix();
+
+                float scale = 0.8f;
+                matrices.scale(scale, scale);
+                String stacksString = String.valueOf(stack.getCount() / 64);
+
+                int scaledX = (int)((x + 16) / scale) - textRenderer.getWidth(stacksString);
+                int scaledY = (int)((y + 1) / scale);
+
+                context.drawText(textRenderer, stacksString, scaledX, scaledY, 0xD0FFFFFF, true);
+
+                matrices.popMatrix();
+            }
+
+        }
+
+        ci.cancel();
+    }
+}
